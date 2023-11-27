aadf8c383a13f48fcd10b6969b7e5864db9f6aa8
roman.makarenko@nzz.ch <roman.makarenko@nzz.ch>
2023-11-27 22:47:48 +0200

    removed not needed js file

--- src/main/webapp/solution.js
+++
@@ -1,9 +0,0 @@
-function getSelectedOption() {
-    var selectedOption = document.querySelector('input[name="options"]:checked');
-
-    if (selectedOption) {
-        alert("Вибраний варіант: " + selectedOption.value);
-    } else {
-        alert("Виберіть варіант");
-    }
-}
